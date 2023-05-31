0829204815ab65e16a83d8378f91b75d648a11a3
BismuthLm <liamislandbay@gmail.com>
2023-06-01 10:51:23 +1200

    oli.js fix

--- src/oli.js
+++ src/oli.js
@@ -1,11 +1,9 @@
-function main(){
- var poop = 2;
- while(poop > 1){
+var poop = 2;
+while(poop > 1){
 
    console.log(poop);
    poop = Math.sqrt(poop)
     poop + 565743663 * 2;
     poop + 1;
     poop = poop * poop * poop;
- }
 }
