f09b6a2ecbf5fe49de967fef304077d92bcba8de
BismuthLm <liamislandbay@gmail.com>
2023-06-01 10:47:10 +1200

    i have taken your child

---
+++ src/oli.js
@@ -0,0 +1,11 @@
+function main(){
+ var poop = 2;
+ while(poop > 1){
+
+   console.log(poop);
+   poop = Math.sqrt(poop)
+    poop + 565743663 * 2;
+    poop + 1;
+    poop = poop * poop * poop;
+ }
+}
