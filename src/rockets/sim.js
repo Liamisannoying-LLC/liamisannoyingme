a26768d43edac6d5a632c12412aad7fa365b35fa
GrumpyTurtl <128335746+GrumpyTurtl@users.noreply.github.com>
2023-06-14 21:45:38 +1200

    adding variables

--- src/rockets/sim.js
+++ src/rockets/sim.js
@@ -1,11 +1,23 @@
 var c = document.getElementById('cvs');
 var ctx = c.getContext("2d");
 
+let thrust = 0;
+let mass = 10000;
+let acceleration = 0;
+let velocity = 0;
+let altitude = 0;
+let airResistance = 0;
+
+const deltaTime = 0.1;
+const initialGravitationalForce = 9.8;
+const gravitationalConstant = 6.67430 * Math.pow(10, -11);
+const earthMass = 5.972 * Math.pow(10, 24);
+const earthRadius = 6371000; 
+
 var playery = 400;
 
 function sim(x,y,r,w,h,thrust,mass){
     playery -= thrust / mass;
-    y = playery;
     ctx.fillRect(x,y,w,h);
     console.log(x,y,r,w,h,thrust,mass,thrust/mass)
 }
