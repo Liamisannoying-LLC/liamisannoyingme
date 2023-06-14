35ca92ca05043b0225f49d44fa78c7c4721c79b4
GrumpyTurtl <Redstonecollector@gmail.com>
2023-06-14 17:27:33 +1200

    vccvcvcvcxjdtx

--- src/rockets/sim.js
+++ src/rockets/sim.js
@@ -1,14 +1,19 @@
 var c = document.getElementById('cvs');
 var ctx = c.getContext("2d");
 
+var playery = 400;
+
 function sim(x,y,r,w,h,thrust,mass){
-    ctx.fillRect(x,y + (thrust / mass),w,h);
+    playery -= thrust / mass;
+    y = playery;
+    ctx.fillRect(x,y,w,h);
+    console.log(x,y,r,w,h,thrust,mass,thrust/mass)
 }
 
 window.requestAnimationFrame(loop);
 
-function loop() {//... its in the name
-    ctx.clearRect(0, 0, c.width, c.height);//clears screen
-    sim(200,400,0,50,50,100,130)
-    window.requestAnimationFrame(loop);//makes it a loop
+function loop() {
+    ctx.clearRect(0, 0, c.width, c.height);
+    sim(200,playery,0,50,50,100,130)
+    window.requestAnimationFrame(loop);
 }
