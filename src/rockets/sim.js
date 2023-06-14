c183b299b5962b3140bf7e3cfd5cd62cfe2034c3
GrumpyTurtl <Redstonecollector@gmail.com>
2023-06-14 17:25:41 +1200

    istanbul not canstanople

--- src/rockets/sim.js
+++ src/rockets/sim.js
@@ -4,3 +4,11 @@ var ctx = c.getContext("2d");
 function sim(x,y,r,w,h,thrust,mass){
     ctx.fillRect(x,y + (thrust / mass),w,h);
 }
+
+window.requestAnimationFrame(loop);
+
+function loop() {//... its in the name
+    ctx.clearRect(0, 0, c.width, c.height);//clears screen
+    sim(200,400,0,50,50,100,130)
+    window.requestAnimationFrame(loop);//makes it a loop
+}
