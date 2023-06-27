200f303723f5e7368c2740d875fbde8c8dcdbdc9
GrumpyTurtl <128335746+GrumpyTurtl@users.noreply.github.com>
2023-06-27 18:06:07 +1200

    SPACE SIM NOW :)

--- src/Games/FutureProject/sim.js
+++ src/Games/FutureProject/sim.js
@@ -1,12 +1,17 @@
 var c = document.getElementById('cvs');
 var ctx = c.getContext("2d");
 
-let thrust = 0;
+c.height = window.innerHeight - 100;
+c.width = window.innerWidth - 100;
+
+let thrust = 0.5;
 let mass = 10000;
 let acceleration = 0;
 let velocity = 0;
 let altitude = 0;
 let airResistance = 0;
+let distanceBetweenCenter;
+let gravitationalPull;
 
 const deltaTime = 0.1;
 const initialGravitationalForce = 9.8;
@@ -15,12 +20,22 @@ const earthMass = 5.972 * Math.pow(10, 24);
 const earthRadius = 6371000; 
 
 var playery = 400;
+var i;
+var speed;
 
 function sim(x,y,r,w,h,thrust,mass){
+    distanceBetweenCenter = 2900 + (playery + h/2);
+    gravitationalPull = (gravitationalConstant * earthMass * mass) / Math.pow(distanceBetweenCenter, 2)
     playery -= thrust / mass;
+    playery -= gravitationalPull/10000000000;
     ctx.fillRect(x,y,w,h);
-    console.log(x,y,r,w,h,thrust,mass,thrust/mass)
+    speed = playery - i;
+    console.log(x,y,r,w,h,thrust,mass,thrust/mass,speed);
+    
 }
+setInterval(function (){
+    i = playery;
+},100);
 
 window.requestAnimationFrame(loop);
 
