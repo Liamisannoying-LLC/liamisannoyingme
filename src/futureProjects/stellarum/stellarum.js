bc46508f53511517e7af6a3bab6a23f5bf4082f2
GrumpyTurtl <redstonecollector@gmail.com>
2023-06-29 10:39:44 +1200

    idk

--- src/futureProjects/stellarum/stellarum.js
+++ src/futureProjects/stellarum/stellarum.js
@@ -4,36 +4,27 @@ var ctx = c.getContext('2d');
 c.width = window.innerWidth - (window.innerWidth / 10);
 c.height = window.innerHeight - (window.innerHeight / 10);
 
-let time = 0;
-const waveSpeed = 0.02; // Controls the speed of the wave
-const waveAmplitude = 50; // Controls the height of the wave
+var waveCount = 1;
+var WavePull = [];
+WavePull.push(c.height/2 + 50);
+
 
 function draw() {
-  // Clear the canvas
+
   ctx.clearRect(0, 0, c.width, c.height);
 
-  // Set the style for the wave
-  ctx.strokeStyle = 'blue';
-  ctx.lineWidth = 2;
+  ctx.fillRect(0,c.height/2,c.width,c.height/2);
 
-  // Start drawing the wave
   ctx.beginPath();
 
-  // Draw each point in the wave
-  for (let x = 0; x <= c.width; x++) {
-    const y = waveAmplitude * Math.sin(x * waveSpeed + time);
-    ctx.lineTo(x, y + c.height / 2);
+  for(var i; i < waveCount; i++){
+    ctx.moveTo(c.width,c.height/2);
+    ctx.lineTo(c.width/5,WavePull[i]);
+    ctx.moveTo(c.width/5,WavePull[i]);
   }
 
-  // Finish drawing the wave
-  ctx.stroke();
 
-  // Update the time for the next frame
-  time += waveSpeed;
-
-  // Request the next animation frame
   requestAnimationFrame(draw);
 }
 
-// Start the animation loop
 draw();
