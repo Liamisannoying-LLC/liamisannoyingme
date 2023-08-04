a1e6273fe2a84763da364b98c1261d47eda58307
GrumpyTurtl <128335746+GrumpyTurtl@users.noreply.github.com>
2023-08-04 22:53:55 +1200

    BOUYANCY FORCE LETSS GOOOOO

--- src/Games/GameEngineKinda/potsu.js
+++ src/Games/GameEngineKinda/potsu.js
@@ -113,28 +113,23 @@ class GameObject {
     }
 }
 
+//kpa
+var PressureList = [101, 99.5, 97.7, 96.0, 94.2, 92.5, 90.8, 89.1, 87.5, 85.9, 84.3, 81.2, 78.2, 75.3, 72.4, 69.7, 57.2, 46.6, 37.6, 30.1, 23.8, 18.7, 14.5, 11.1]
+//meters
+var PressureAlt = [0, 152, 305, 457, 610, 762, 914, 1067, 1219, 1372, 1524, 1829, 2134, 2438, 2743, 3048, 4572, 6096, 7620, 9144, 10668, 12192, 13716, 15240]
 
-//const Gravity = 9.8;
+function boyancyForce(balloonRadius, altitude){//meters
+    let balloonVolume = 4/3 * Math.PI * Math.pow(balloonRadius, 3);//meters^3
+    let airDensity;//kg/m^3
 
+    for(let i = 0; i < PressureAlt.length; i++){//finds air pressure of altitude
+        if(altitude > PressureAlt[i] && altitude < PressureAlt[i+1]){
+            airDensity = PressureList[i];
+        }
+    }
 
-// function boyancyForce(airDensity, hotAirTemp, pressureInsideBalloon, balloonRadius) {
-//     const molarMassAir = 0.029;
-//     const gasConstant = 8.314;
-//     //https://www.omnicalculator.com/physics/air-density
-
-//     // Calculate hot air density using the Ideal Gas Law
-//     const hotAirDensity = (pressureInsideBalloon * molarMassAir) / (gasConstant * hotAirTemp);
-
-//     // Calculate the volume of the hot air balloon (assuming spherical shape)
-//     const balloonVolume = (4 / 3) * Math.PI * Math.pow(balloonRadius, 3);
-
-//     // Calculate the buoyant force
-//     const buoyantForce = (airDensity - hotAirDensity) * balloonVolume * Gravity;
-
-//     return buoyantForce;
-// }
-
-
+    return balloonVolume * airDensity * 9.8;//bf = V x D * G
+}
 
 
 //SATTTTTTTTTTTTTTTTT stolen from the interweb
