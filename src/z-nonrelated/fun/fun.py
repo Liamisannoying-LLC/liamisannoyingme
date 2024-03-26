02a62bf62188788dc4709396eeed183028ad69be
BismuthLm <liamislandbay@gmail.com>
2024-03-26 17:03:32 +1300

    ahhhh python

--- src/z-nonrelated/fun/fun.py
+++ src/z-nonrelated/fun/fun.py
@@ -1,11 +1,20 @@
+def startGame():
+    print("""---
+          This is a game originally by Liam Mitchell, 2024.
+          This material does not have a licence. If a future version does have one, that version should be considered seperate from this one.
+          More information should be here soon, if you happen to stumble on this before the next update.
+             ---
+""")
+    hall()
+
 def noGo():
     print("""You can't go there...
           """)
 
 def hall():
-    print("""Welcome to the hall"""
-          )
-    nextMove = input("What now?")
+    print("Welcome to the hall")
+    print("What now?")
+    nextMove = input(">")
     if nextMove == ("north"):
         noGo()
     elif nextMove == ("east"):
@@ -14,11 +23,13 @@ def hall():
         noGo()
     elif nextMove == ("west"):
         kitchen()
+    else:
+        print("I don't recognise that command. Try help() for controls.")
 
 def bedroom():
-    print("""You are now in the bedroom
-          """)
-    nextMove = input("What now?")
+    print("You are now in the bedroom")
+    print("")
+    nextMove = input("""What now?""")
     if nextMove == ("north"):
         noGo()
     elif nextMove == ("east"):
@@ -29,9 +40,9 @@ def bedroom():
         hall()
 
 def kitchen():
-    print("""You find yourself in the kitchen
-          """)
-    nextMove = input("What now?")
+    print("You find yourself in the kitchen")
+    print("")
+    nextMove = input("""What now?""")
     if nextMove == ("north"):
         noGo()
     elif nextMove == ("east"):
