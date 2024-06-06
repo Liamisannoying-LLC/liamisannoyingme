312e2df1569e7d481622546deec3773ced17292f
Li Mi <liamislandbay@gmail.com>
2024-06-07 09:13:52 +1200

    scalabity

--- src/z-nonrelated/work/y10work/4.js
+++ src/z-nonrelated/work/y10work/4.js
@@ -10,8 +10,7 @@
 
 console.log("Loop activity")
 
-const WIDTH = 300
-const HEIGHT = 300
+const WIDTH = 600
 
 var ctx
 
@@ -22,7 +21,7 @@ window.onload=startCanvas
 function startCanvas(){
 	ctx=document.getElementById("myCanvas").getContext("2d")
 
-//Following draws the green squares, manually. 
+//Following draws the green squares, using a nested loop so it can be sclable.
 
 	ctx.fillStyle = "blue"
 	console.log("Starting blue")
@@ -37,91 +36,38 @@ function startCanvas(){
 		xPosition = xPosition + 100
 	}
 
-	//Following draws the green squares, using a few nearly identical loops.
+	//Following draws the green squares, using a nested loop so it can be sclable.
 
 	ctx.fillStyle = "green"
 	console.log("Starting green")
-
+	
 	xPosition = 50
-	count = 0
-	while(count < 6){
-    	ctx.fillRect(xPosition, 0, 50, 50)
+	while(xPosition < WIDTH){ 
+		yPosition = 0
+		while(yPosition < WIDTH){ 
+			ctx.fillRect(xPosition, yPosition, 50, 50)
+			yPosition = yPosition + 100
+		}
 		xPosition = xPosition + 100
-		count = count + 1
-	}
-	xPosition = 50
-	count = 0
-	while(count < 6){
-    	ctx.fillRect(xPosition, 100, 50, 50)
-		xPosition = xPosition + 100
-		count = count + 1
-	}
-	xPosition = 50
-	count = 0
-	while(count < 6){
-    	ctx.fillRect(xPosition, 200, 50, 50)
-		xPosition = xPosition + 100
-		count = count + 1
-	}
-	xPosition = 50
-	count = 0
-	while(count < 6){
-    	ctx.fillRect(xPosition, 300, 50, 50)
-		xPosition = xPosition + 100
-		count = count + 1
-	}
-	xPosition = 50
-	count = 0
-	while(count < 6){
-   		ctx.fillRect(xPosition, 400, 50, 50)
-		xPosition = xPosition + 100
-		count = count + 1
-	}
-	xPosition = 50
-	count = 0
-	while(count < 6){
-   		ctx.fillRect(xPosition, 500, 50, 50)
-		xPosition = xPosition + 100
-		count = count + 1
 	}
 
-	//Following draws the orange squares, using the same method as the green squares, except instead of a count variable, we use WIDTH
+	//Following draws the orange squares, using a nested loop so it can be sclable.
 
 	ctx.fillStyle = "orange"
 	console.log("Starting orange")
+	
+	xPosition = 0
+	while(xPosition < WIDTH){ 
+		yPosition = 50
+		while(yPosition < WIDTH){ 
+			ctx.fillRect(xPosition, yPosition, 50, 50)
+			yPosition = yPosition + 100
+		}
+		xPosition = xPosition + 100
+	}
+		
 
-	xPosition = 0
-	while(xPosition < WIDTH){
-		ctx.fillRect(xPosition, 50, 50, 50)
-		xPosition = xPosition + 100
-	}
-	xPosition = 0
-	while(xPosition < WIDTH){
-		ctx.fillRect(xPosition, 150, 50, 50)
-		xPosition = xPosition + 100
-	}
-	xPosition = 0
-	while(xPosition < WIDTH){
-		ctx.fillRect(xPosition, 250, 50, 50)
-		xPosition = xPosition + 100
-	}
-	xPosition = 0
-	while(xPosition < WIDTH){
-		ctx.fillRect(xPosition, 350, 50, 50)
-		xPosition = xPosition + 100
-	}
-	xPosition = 0
-	while(xPosition < WIDTH){
-		ctx.fillRect(xPosition, 450, 50, 50)
-		xPosition = xPosition + 100
-	}
-	xPosition = 0
-	while(xPosition < WIDTH){
-		ctx.fillRect(xPosition, 550, 50, 50)
-		xPosition = xPosition + 100
-	}
-
-	//Following draws the purple squares, using nested loop, with WIDTH instead of count
+	//Following draws the purple squares, using a nested loop so it can be sclable.
 
 	ctx.fillStyle = "purple"
 	console.log("Starting purple")
@@ -136,7 +82,7 @@ function startCanvas(){
 		xPosition = xPosition + 100
 	}
 
-	//Following draws the red squares, using nested loop
+	//Following draws the red squares, using a nested loop so it can be sclable.
 
 	ctx.fillStyle = "red"
 	console.log("Starting red")
@@ -151,31 +97,25 @@ function startCanvas(){
 		xPosition = xPosition + 50
 	}
 
-	//Following draws the black squares, using two non-nested loops becuase making them nested was unnecessary for the product (for now)
+	//Following draws the black squares, using a nested loop so it can be sclable.
 
 	ctx.fillStyle = "black"
 	console.log("Starting black")
 	
-	xPosition = 30;
-	yPosition = 30;
-	blacksquarecount = 0
-	blacksquaresdone = false
-	
-	while(blacksquarecount < 11){
-		ctx.fillRect(xPosition, yPosition, 40, 40)
-		xPosition = xPosition + 50
-		yPosition = yPosition + 50
-		blacksquarecount = blacksquarecount + 1
-	}
-	xPosition = 530
-	yPosition = 30
-	blacksquarecount = 0
+	leftXPosition = 30
+	YPosition = 30
+	rightXPosition = WIDTH - 70
+	blackDone = false
 
-	while (blacksquarecount < 11){
-		ctx.fillRect(xPosition, yPosition, 40, 40)
-		xPosition = xPosition - 50
-		yPosition = yPosition + 50
-		blacksquarecount = blacksquarecount + 1
+	if(blackDone == false){
+		ctx.fillRect(leftXPosition, YPosition, 40, 40)
+		ctx.fillRect(rightXPosition, YPosition, 40, 40)
+		leftXPosition = leftXPosition + 50
+		yPosition = yPosition - 50
+		ctx.fillRect(leftXPosition, YPosition, 40, 40)
+		ctx.fillRect(rightXPosition, YPosition, 40, 40)
+
+		blackdone = true
 	}
 
 console.log("Finished")
