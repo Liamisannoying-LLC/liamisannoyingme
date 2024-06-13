d9a4325adda8c5710bfc21efd23879f0773f9be2
Li Mi <liamislandbay@gmail.com>
2024-06-14 11:28:16 +1200

    more words

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -25,7 +25,7 @@ var corpus = [
 ];
 function generateWords(){ 
   var correctLanguage = "";
-  for (i = 0; i < 3; i++) {
+  for (i = 0; i < 100000; i++) {
     index = Math.floor(Math.random() * corpus.length);
     correctLanguage = correctLanguage + " " + corpus[index];}
   document.getElementById('PoemDisplay').textContent = correctLanguage;}
